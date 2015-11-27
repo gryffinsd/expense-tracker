75fe6c164c316dfd5b75afc3f3362e1a3c7a218e
Pradnyesh Sawant <spradnyesh@gmail.com>
2015-11-27 13:56:54 +0530

    fix: [bug] reset! g/app-page not working

--- src/cljs/expense_tracker/menu.cljs
+++ src/cljs/expense_tracker/menu.cljs
@@ -1,6 +1,9 @@
 (ns expense-tracker.menu
   (:require [expense-tracker.globals :as g]))
 
+;; reset-app
+(defn ra [e m] (reset! g/app-page m) nil)
+
 (defn c-menu []
   [:div.navbar.navbar-default
    [:div.container-fluid
@@ -8,8 +11,8 @@
      [:a.navbar-brand {:href "#"} "Expense Tracker"]]
     [:div.collapse.navbar-collapse
      [:ul.nav.navbar-nav
-      [:li [:a {:href "#" :onClick #(reset! g/app-page {:page :home})} "Home"]]
-      [:li [:a {:href "#" :onClick #(reset! g/app-page {:page :trans-add})} "Add Transaction"]]
+      [:li [:a {:href "#" :onClick #(ra % {:page :home})} "Home"]]
+      [:li [:a {:href "#" :onClick #(ra % {:page :trans-add})} "Add Transaction"]]
       [:li.dropdown [:a.dropdown-toggle {:href "#"
                                          :data-toggle "dropdown"
                                          :role "button"
@@ -17,8 +20,8 @@
                                          :aria-expanded "false"}
                      "Accounts" [:span.caret]]
        [:ul.dropdown-menu
-        [:li [:a {:href "#" :onClick #(reset! g/app-page {:page :acc-view})} "View"]]
-        [:li [:a {:href "#" :onClick #(reset! g/app-page {:page :acc-add})} "Add New"]]]]
+        [:li [:a {:href "#" :onClick #(ra % {:page :acc-view})} "View"]]
+        [:li [:a {:href "#" :onClick #(ra % {:page :acc-add})} "Add New"]]]]
       [:li.dropdown [:a.dropdown-toggle {:href "#"
                                          :data-toggle "dropdown"
                                          :role "button"
@@ -26,6 +29,6 @@
                                          :aria-expanded "false"}
                      "Reports by" [:span.caret]]
        [:ul.dropdown-menu
-        [:li [:a {:href "#" :onClick #(reset! g/app-page {:page :rep-pie})} "Time Period"]]
+        [:li [:a {:href "#" :onClick #(ra % {:page :rep-pie})} "Time Period"]]
         #_[:li.divider {:role "separator"}]
-        [:li [:a {:href "#" :onClick #(reset! g/app-page {:page :rep-bar})} "Account"]]]]]]]])
+        [:li [:a {:href "#" :onClick #(ra % {:page :rep-bar})} "Account"]]]]]]]])
